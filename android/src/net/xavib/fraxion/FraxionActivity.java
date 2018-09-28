da5651cc944a26ac2d82703eaae2d0ca3f73f2d2
xavi-b <xavi-b@users.noreply.github.com>
2018-09-29 00:57:20 +0200

    AdMob on start

--- android/src/net/xavib/fraxion/FraxionActivity.java
+++ android/src/net/xavib/fraxion/FraxionActivity.java
@@ -4,17 +4,62 @@ import android.app.Notification;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.os.Vibrator;
+import android.os.Bundle;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import org.qtproject.qt5.android.bindings.QtActivity;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.InterstitialAd;
 
 public class FraxionActivity extends QtActivity
 {
     private static FraxionActivity fraxionActivity;
 
+    private InterstitialAd _interstitialAd;
+
     public FraxionActivity()
     {
         fraxionActivity = this;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        MobileAds.initialize(this, "ADMOB_APP_ID");
+        _interstitialAd = new InterstitialAd(this);
+        _interstitialAd.setAdUnitId("ADMOB_UNIT_ID");
+        _interstitialAd.loadAd(new AdRequest.Builder().build());
+
+        _interstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdLoaded() {
+                _interstitialAd.show();
+            }
+
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+
+            }
+
+            @Override
+            public void onAdOpened() {
+
+            }
+
+            @Override
+            public void onAdLeftApplication() {
+
+            }
+
+            @Override
+            public void onAdClosed() {
+
+            }
+        });
     }
 
     public static void notify(String content)
--- main.cpp
+++ main.cpp
@@ -4,7 +4,6 @@
 #include "FraxionActivity.h"
 
 // TODO
-// ringtone
 // settings
 
 int main(int argc, char *argv[])
